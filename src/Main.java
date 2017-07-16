af3352dad3304e6dd5dc0a5ba2bcd10518052597
Cvazer <zhirnitoni@gmail.com>
2017-07-16 14:56:21 +0300

    mark

--- src/Main.java
+++ src/Main.java
@@ -1,92 +1,61 @@
 import javax.swing.*;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Formatter;
 import java.util.Scanner;
 
 public class Main {
-    static Scanner scn;
+    private static Scanner scn;
+    private static boolean running = true;
     private static ArrayList<Student> students = new ArrayList<>();
 
     public static void main(String[] args) {
-        openfile();
-        readfile();
-        writerfile();
-
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            String s = "";
-            if (scanner.hasNext()) {
-                s = scanner.next();
-            }
-            if (s.toUpperCase().contentEquals("EXIT")) {
-                break;
-            } else if (s.toUpperCase().contentEquals("ADD")) {
-                students.add(new Student(scanner.next(), scanner.nextInt()));
-            } else if (s.toUpperCase().contentEquals("REMOVE")) {
-                System.out.println("ready to remove student");
-                students.remove(find(scanner.nextInt()));
-            } else if (s.toUpperCase().contentEquals("PRINT")) {
-                for (Student student : students) {
-                    System.out.println(student);
-                }
-            } else if (s.toUpperCase().contentEquals("EDIT")) {
-                System.out.println("ready to edit student");
-                find(scanner.nextInt()).setName(scanner.next());
-            } else {
-                System.out.println("UNKNOWN COMMAND!");
+        loadData();
+        scn = new Scanner(System.in);
+        while (running){
+            String c = scn.nextLine();
+            switch (c.toUpperCase()){
+                case "EXIT":
+                    running = false;
+                    break;
+                case "ADD":
+                    students.add(new Student(scn.next(), scn.nextInt()));
+                    break;
+                case "PRINT":
+                    students.forEach(System.out::println);
+                    break;
+                default:
+                    System.out.println("ERROR");
+                    break;
             }
         }
+        saveData();
     }
 
-    //Идентификация
-    private static Student find(int id) {
-        Student r_student = null;
-        for (Student student : students) {
-            if (id == student.getId()) {
-                r_student = student;
-                break;
-            }
-        }
-        return r_student;
-    }
-
-    private static void openfile() {
+    private static void saveData() {
+        PrintWriter writer = null;
         try {
-            scn = new Scanner(new File("res//Save.txt"));
-        } catch (Exception e) {
-            JOptionPane.showMessageDialog(null, "файл не найден");
-        }
-    }
-
-
-    //чтение файла
-    private static void readfile() {
-        while (scn.hasNext()) {
-            for (int row = 0; row < students.length; row++) {
-                for (int col = 0; col < students[row].lenght; col++) {
-                    students[row][col] = scn.hasNext();
-                }
-            }
-        }
-    }
-
-    private static void writerfile() {
-        try (FileWriter writer = new FileWriter("res//Save.txt", false)) {
-            // запись всей строки
-            //String text = "Мама мыла раму, раму мыла мама";
-            writer.write(students);
-            // запись по символам
-            writer.append('\n');
-            writer.append('E');
-
-            writer.flush();
-        } catch (IOException ex) {
-
-            System.out.println(ex.getMessage());
-        } catch (IOException e) {
+            writer = new PrintWriter(new File("res/Save.txt"));
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
+        }
+        String s = "";
+        for (Student student: students){
+            s += student.getName()+" "+student.getId()+"\n";
+        }
+        writer.print(s);
+        writer.close();
+    }
+
+    private static void loadData() {
+        try {
+            scn = new Scanner(new File("res/Save.txt"));
+        } catch (FileNotFoundException e) {
+            JOptionPane.showMessageDialog(null, "Файл не найден");
+            e.printStackTrace();
+        }
+        while (scn.hasNext()) {
+            students.add(new Student(scn.next(), scn.nextInt()));
         }
     }
 }
